b4e5798ff3123d238bb08d639b9d65018654d759
Anshad UK <anshadu@gmail.com>
2024-12-04 23:57:04 +0530

    add readme

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_exercise.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
